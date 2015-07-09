ce15c0aac0488a4d1f09cf81f85f3d49b8f662f3
rmatusiak <radoslaw.matusiak@stazysta.comarch.pl>
2015-07-09 15:54:46 +0200

    Add document adnotation for Mongo

--- src/main/java/com/acme/order/customer/Customer.java
+++ src/main/java/com/acme/order/customer/Customer.java
@@ -9,6 +9,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Document
 @Table(name = "customer_t")
 public class Customer {
 	@Id
